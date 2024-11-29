761bb3415ac8c14e21c2ba7fc02854bf42e1d29e
Hosam-hsm <tbthsm1@gmail.com>
2024-11-30 01:32:17 +0530

    Added Spring Boot application to CICD Project folder

---
+++ CICD Project/src/main/java/com/app/proectappforcicd/ProectappforcicdApplication.java
@@ -0,0 +1,13 @@
+package com.app.proectappforcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProectappforcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProectappforcicdApplication.class, args);
+	}
+
+}
---
+++ CICD Project/src/test/java/com/app/proectappforcicd/ProectappforcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.proectappforcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProectappforcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
